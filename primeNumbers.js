0cf0ebb0ddfb4634d0bfc237d6d95a99bd8357b8
mary <Mariashee001@gmail.com>
2024-04-01 23:37:41 +0300

    complete wk2 code challenge

--- primeNumbers.js
+++
@@ -1,7 +0,0 @@
-function isPrime(number) {
-    // 2 is the 1st prime number so anything below that is not a prime number
-    if (number < 2) {
-        return false;
-    }
-    // 2 will be like our first
-}
